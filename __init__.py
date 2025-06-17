dc3292ca173a2149825ab21b96db719beaf37a7d
Léo Picat <leo.n.picat@gmail.com>
2025-06-17 12:37:06 +0200

    Add a license (AGPL-3.0)

--- __init__.py
+++ __init__.py
@@ -1,3 +1,19 @@
+# Deck icons, add icons to the deck window in Anki
+# Copyright (C) 2025  Léo Picat
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # import the main window object (mw) from aqt
 from aqt import mw
 # import the "show info" tool from utils.py
